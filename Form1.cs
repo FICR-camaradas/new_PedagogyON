0e59d68f691675d1e43310ae8c08fdd5b61546dd
ikerlyson <jose.ikerlyson@a.ficr.edu.br>
2021-06-07 15:12:23 -0300

    adicionada a biblioteca Newtonsoft.Json e referenciada nos forms existentes

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;  // Fornece classes para diversos protocolos de rede
+using System.IO;   // Fornece classes para ler e gravar arquivos e fluxo de dados
+using Newtonsoft.Json;
+
 namespace PedagogyOn_2021
 {
-    partial class Form1
+    partial class Form1 : Form
     {
         /// <summary>
         /// Variável de designer necessária.
--- PerfilUsuario2.cs
+++ PerfilUsuario2.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;  // Fornece classes para diversos protocolos de rede
+using System.IO;   // Fornece classes para ler e gravar arquivos e fluxo de dados
+using Newtonsoft.Json;
+
 
 namespace PedagogyOn_2021
 {
